9ee3ee49f522321c70fc606eda8618a5b0587027
René Wilhelm <rene.wilhelm@gmail.com>
2014-02-11 01:52:13 +0100

    Simplified query. Creating FeatureCollection in har.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ if ('development' == app.get('env')) {
 function gps(params, body, callback) {
 	db.connect("pg://postgres:liveandgov@localhost/liveandgov", function(err, client, done) {
 		var statement = {
-			text: "SELECT row_to_json(fc) FROM (SELECT 'FeatureCollection' AS type, array_to_json(array_agg(f)) AS features FROM (SELECT 'Feature' AS type, ST_AsGeoJSON(ST_MakeLine(geom::geometry, geom2::geometry))::json AS geometry, row_to_json((SELECT l FROM (SELECT trip_id as id, ts) AS l)) AS properties FROM (SELECT trip_id, ts, lonlat as geom,	lead(lonlat) OVER w AS geom2 FROM sensor_gps WHERE trip_id = $1 WINDOW w AS (PARTITION BY trip_id ORDER BY ts) ) AS q WHERE geom2 IS NOT NULL ) AS f ) AS fc",
+			text: "SELECT ts, ST_AsGeoJSON(lonlat)::json AS lonlat FROM sensor_gps WHERE trip_id = $1",
 			values: [params.trip_id]
 		};
 
