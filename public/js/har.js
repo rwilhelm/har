baca144d38acd00e910c21113ac951abb56b970d
René Wilhelm <rene.wilhelm@gmail.com>
2014-02-11 01:55:00 +0100

    Reorganized and changed some things. Generating FeatureCollection and appending matching polylines.

--- public/js/har.js
+++ public/js/har.js
@@ -1,63 +1,107 @@
+'use strict';
+
+// var gju = require('geojson-utils');
+
 var API_KEY = 'BC9A493B41014CAABB98F0471D759707'
 
 var map = new L.Map("map", {zoom: 15, minZoom: 12, maxZoom: 18})
-		.addLayer(new L.TileLayer("http://{s}.tile.cloudmade.com/" + API_KEY + "/998/256/{z}/{x}/{y}.png"));
+	.addLayer(new L.TileLayer("http://{s}.tile.cloudmade.com/" + API_KEY + "/998/256/{z}/{x}/{y}.png"));
 
-var data;
-var activities;
-
-// Query the sensor_gps table to get the route
+var fc, activities;
 d3.json('/' + trip_id + '/gps', function(collection) {
 	if (!collection) { console.log("NO GPS DATA!"); return };
+	var gps = collection;
 
-	var geoJson = data = collection[0].row_to_json;
-
-	// Query the har_annotation table to get the activity tags
 	d3.json('/' + trip_id + '/har', function(collection) {
 		if (!collection) { console.log("NO HAR DATA!"); return };
+		var har = collection;
 
-		for (var i=0; i<geoJson.features.length; i++) {
-			if (geoJson.features[i+1] !== undefined) {
-				var t0 = geoJson.features[i].properties.ts;
-				var t1 = geoJson.features[i+1].properties.ts;
+		fc = {
+			"type": "FeatureCollection",
+			"features": []
+		};
+
+		var geoJson;
+
+		var n = 0;
+		for (var i=0; i<gps.length; i++) {
+			var t0 = gps[i].ts;
+			var g0 = gps[i].lonlat.coordinates; // array
+
+			if (gps[i+1]) {
+				var t1 = gps[i+1].ts;
+				var g1 = gps[i+1].lonlat.coordinates; // array
+
 				var a = [];
-				// Get all activities matching a feature/between two timestamps
-				collection.map(function(d) { if (d.ts > t0 && d.ts < t1) { a.push(d.tag) } })
-				// Save the most occuring activity to the feature properties
-				geoJson.features[i].properties.activity = getMaxOccurrence(a);
+				har.map(function(d) { if (d.ts > t0 && d.ts < t1) { a.push(d.tag) } })
+				a = getMaxOccurrence(a);
 			}
+
+			if (prev && a == prev.properties.activity) {
+				prev.geometry.coordinates.push(g1);
+				prev.properties.t1 = t1;
+				prev.properties.distance += calculateDistance(g0, g1);
+				prev.properties.duration += moment.duration(t1-t0);
+			} else {
+				fc.features.push({
+					"type": "Feature",
+					"geometry": {
+						"type": "LineString",
+						"coordinates": [ g0, g1 ]
+					},
+					"properties": {
+						"id": trip_id,
+						"n": n,
+						"t0": t0,
+						"t1": t1,
+						"duration": moment.duration(t1-t0),
+						"activity": a,
+						"distance": calculateDistance(g0, g1)
+					}
+				});
+				n++;
+			}
+
+			if (fc.features.length > 0) { var prev = fc.features[fc.features.length-1] }
 		}
 
-		activities = geoJson.features.map(function(d) { return d.properties.activity })
-			.sort().filter(function(el,i,a) { if (i == a.indexOf(el)) return 1; return 0 })
-			.filter(function(n) { return n });
+		activities = fc.features.map(function(d) { return d.properties.activity })
+			.sort().filter(function(el,i,a) { if (i == a.indexOf(el)) return 1; return 0 });
+
+		activities[activities.indexOf(null)] = "unknown";
+		activities.filter(function(n) { return n });
+
+		function knownActivities(array) {
+			a = ["running", "walking", "standing", "sitting", "on table", "unknown"];
+			return a.map(function(d) { return array[array.indexOf(d)] });
+		}
 
 		function getColor(d) {
-		switch (d) {
-				case 'standing': return "#0DFB8F"; // green
+			switch (d) {
 				case 'walking':  return "#E0BC0B"; // yellow
 				case 'running':  return "#F60100"; // red
-				case 'sitting':  return "#6B82E0"; // blue
-				case 'on table': return "#333333"; // grey
+				case 'standing': return "#222222"; // green
+				case 'sitting':  return "#666666"; // blue
+				case 'on table': return "#999999"; // grey
+				case 'unknown': return "#ff00ff"; // grey
+				case null: return "#ff00ff"; // grey
 			}
 		}
 
 		function style(feature) {
 			return {
 				weight: 8,
-				opacity: 0.3,
+				opacity: 0.5,
 				color: getColor(feature.properties.activity),
 			};
 		}
 
-		// Called by onEachFeature/layer.on
 		function highlightFeature(e) {
 			var layer = e.target;
 
 			layer.setStyle({
-				weight: 8,
-				opacity: 0.7,
-				color: '#f00',
+				weight: 16,
+				opacity: 0.8
 			});
 
 			if (!L.Browser.ie && !L.Browser.opera) {
@@ -69,10 +113,13 @@ d3.json('/' + trip_id + '/gps', function(collection) {
 
 		legend.onAdd = function (map) {
 			var div = L.DomUtil.create('div', 'info legend leaflet-bar');
-			for (var i = 0; i < activities.length; i++) {
+
+			a = knownActivities(activities).filter(function(n) { return n });
+
+			for (var i = 0; i < a.length; i++) {
 				div.innerHTML +=
-					'<i style="background:' + getColor(activities[i]) + '"></i>' +
-					activities[i] + '<br>';
+					'<i style="background:' + getColor(a[i]) + '"></i>' +
+					a[i] + '<br>';
 			}
 			return div;
 		};
@@ -87,7 +134,9 @@ d3.json('/' + trip_id + '/gps', function(collection) {
 				var popupString = '<div class="popup">';
 				for (var k in feature.properties) {
 					var v = feature.properties[k];
-					(k == 'ts') ? v = moment.unix(v).format("HH:MM:SS") : null;
+					(k == 't0' || k == 't1') ? v = moment.unix(parseInt(v)).utc().format("HH:mm:ss") : null;
+					(k == 'distance') ? v = Math.round(v*100, 12) / 100 + " m" : null;
+					(k == 'duration') ? v = moment.duration(v).humanize() : null;
 					popupString += k + ': ' + v + '<br />';
 				}
 				popupString += '</div>';
@@ -102,23 +151,20 @@ d3.json('/' + trip_id + '/gps', function(collection) {
 			});
 		}
 
-		// Add geoJSON object to the map
-		geoJson = L.geoJson(geoJson, {
+		// Draw geoJSON object to map
+		geoJson = L.geoJson(fc, {
 			style: style,
 			onEachFeature: onEachFeature
 		}).addTo(map);
 
+		// Add legend to map
 		legend.addTo(map);
 
 		// Zoom map to fit our route
 		map.fitBounds(geoJson.getBounds());
 
-	//END d3_har
-	});
+	// HELPERS
 
-	// Helper function to get the array element with the most occurences. Used
-	// for majority voting to apply activity tags to features.
-	// http://stackoverflow.com/a/1053865/220472
 	function getMaxOccurrence(array) {
 		if(array.length == 0)
 			return null;
@@ -140,5 +186,11 @@ d3.json('/' + trip_id + '/gps', function(collection) {
 		return maxEl;
 	}
 
+	function calculateDistance(a, b) {
+		return gju.pointDistance({type: 'Point', coordinates: a}, {type: 'Point', coordinates: b})
+	}
+
+	//END d3_har
+	});
 //END d3_gps
 });
